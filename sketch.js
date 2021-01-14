b35abfb21c38d3af8651b608ee4b2f0bea1c6b4c
Reyan10 <74668221+Reyan10@users.noreply.github.com>
2021-01-14 20:00:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine,world;
+	
+var paperObject
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    paperObject = new Paper(100, 100, 40);
+    
+	box1 = new Box(650,640,200,200);
+   
+
+     ground = new Ground(400, 670, 800, 20)
+
+
+	 Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  box1.display();
+  paperObject.display();
+  ground.display();
+ 
+ 
+
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+   
+   Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:55,y:-80});
+   // packageBody.restitution=0.8;
+	 
+	}
+   }
+   
+
+
